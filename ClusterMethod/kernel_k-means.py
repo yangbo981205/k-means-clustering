6abe55475fb53f3cb73708d7c616b9de5ce43a84
杨博 <2224383456@qq.com>
2021-05-19 17:25:52 +0800

    Update kernel_k-means.py

--- ClusterMethod/kernel_k-means.py
+++ ClusterMethod/kernel_k-means.py
@@ -12,7 +12,7 @@ from sklearn import metrics
 
 # 生成数据集
 def make_data():
-    return datasets.make_circles(n_samples=200, factor=.5, noise=.05)
+    return datasets.make_circles(n_samples=200, factor=0.5, noise=0.05)
 
 
 # 数据集处理
@@ -41,7 +41,8 @@ def show_data(data, result):
 def axes3d(data):
     new_data_list = []
     for d in data:
-        new_data = [np.sqrt(d[0]**2+d[1]**2), d[0], d[1]]
+        new_data = [2*d[0]**2+2*d[1]**2, d[0]+1, d[1]+1]
+        # new_data = [np.sqrt(d[0]**2+d[1]**2), d[0]+1, d[1]+1]
         new_data_list.append(new_data)
 
     return new_data_list
@@ -59,6 +60,78 @@ def show_data_3d(data, result):
     plt.show()
 
 
+# 三维迭代图像绘制
+def show_data_iter_3d(data, center):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for cls_d in data:
+        for d in cls_d:
+            if data.index(cls_d) == 0:
+                ax.scatter(d[0], d[1], d[2], c="r", marker="8")
+            if data.index(cls_d) == 1:
+                ax.scatter(d[0], d[1], d[2], c="g", marker="8")
+
+    for c in center:
+        if center.index(c) == 0:
+            ax.scatter(c[0], c[1], c[2], c="r", marker="v")
+        if center.index(c) == 1:
+            ax.scatter(c[0], c[1], c[2], c="g", marker="v")
+
+    plt.show()
+
+
+# k-means类中心的选择
+def select_centre(data, k):
+    center_list = []
+    for i in range(k):
+        ran = np.random.randint(len(data))
+        center_list.append(data[ran])
+    return center_list
+
+
+# k-means聚类
+def k_means(data, center):
+    new_center = []
+    cluster_result = []
+    class_result = [[] for i in range(len(center))]
+    for d in data:
+        dis_list = []
+        for c in center:
+            # 欧氏距离进行距离度量
+            dis = np.sqrt(np.sum(np.square(np.array(d)-np.array(c))))
+            dis_list.append(dis)
+        cluster_result.append(dis_list.index(min(dis_list)))
+        class_result[dis_list.index(min(dis_list))].append(d)
+
+    # 更新类中心
+    for cls in class_result:
+        x = 0
+        y = 0
+        z = 0
+        for c in cls:
+            x += c[0]
+            y += c[1]
+            z += c[2]
+        if len(cls) == 0:
+            new_center.append(center[class_result.index(cls)])
+        else:
+            new_center.append([round(x/len(cls), 4), round(y/len(cls), 4), round(z/len(cls), 4)])
+
+    print(new_center)
+    # print(len(class_result[0]), len(class_result[1]), len(class_result[2]))
+
+    return cluster_result, class_result, new_center
+
+
+# 迭代
+def iter_(data, center, iter):
+    cluster_result, class_result, new_center = k_means(data, center)
+    for i in range(iter-1):
+        cluster_result, class_result, new_center = k_means(data, new_center)
+        show_data_iter_3d(class_result, new_center)
+    return cluster_result, class_result, new_center
+
+
 if __name__ == '__main__':
     # 生成数据集
     datas = make_data()
@@ -73,5 +146,10 @@ if __name__ == '__main__':
     # print(data_3d)
     # 绘制三维图像
     show_data_3d(data_3d, result)
+
+    # 选择k-means的类中心点
+    centers = select_centre(data_3d, 2)
+
+    cluster_results, class_results, new_centers = iter_(data_3d, centers, 5)
 
 
